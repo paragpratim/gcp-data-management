31c53d1ffa7e8ffe7d5020472106689dacabbd1a
Parag Ghosh <paragpratim@gmail.com>
2025-05-04 00:55:56 +0200

    Added Sample Data

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,9 @@ resource "google_storage_bucket" "bucket_landing_zone" {
   name     = "data-landing-zone-${var.project_id_data}"
   location = var.region
   provider = google.data-plane
+  provisioner "local-exec" {
+    command = "gsutil cp -r ../sample-data/ gs://${google_storage_bucket.bucket_landing_zone.name}" 
+  }
 }
 
 resource "google_storage_bucket" "bucket_raw_zone" {
