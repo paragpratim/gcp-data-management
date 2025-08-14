ef10589074118c67b2b62ae281655614c587dc2c
Parag Ghosh <paragpratim@gmail.com>
2025-08-14 17:18:32 +0200

    CHanged the APP to react.

--- ingestion-ui/public/config.js
+++
@@ -1,3 +0,0 @@
-window.API_CONFIG = {
-  BASE_URL: "http://localhost:8080" // Change this per environment
-};
---
+++ ingestion-ui/src/config.js
@@ -0,0 +1,4 @@
+const API_CONFIG = {
+  BASE_URL: "http://localhost:8080"
+};
+export default API_CONFIG;
