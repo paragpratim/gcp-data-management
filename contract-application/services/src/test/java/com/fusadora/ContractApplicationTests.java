d876c47c783c059931d41fc17837448cc8e33e63
Parag Ghosh <paragpratim@gmail.com>
2025-11-20 16:30:59 +0100

    fix: Enhance ContractApplicationTests with context initialization and bean presence checks

--- contract-application/services/src/test/java/com/fusadora/ContractApplicationTests.java
+++ contract-application/services/src/test/java/com/fusadora/ContractApplicationTests.java
@@ -1,14 +1,26 @@
 package com.fusadora;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ContractApplicationTests {
 
-	@Test
-	void contextLoads() {
-        // TODO document why this method is empty
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Test
+    void applicationContextStarts() {
+        assertNotNull(ctx, "ApplicationContext should have been initialized");
+    }
+
+    @Test
+    void contractApplicationBeanPresent() {
+        assertNotNull(ctx.getBean(ContractApplication.class), "ContractApplication bean should be present in the context");
     }
 
 }
